abec3c119353087bb527a11ccaf7bedd3fc9afb5
Aser <adocteur@gmail.com>
2024-01-01 20:48:49 -0500

    Edit: Searching Books Input

--- client/BOOKSTOREUI/src/Types.tsx
+++
@@ -1,9 +0,0 @@
-export 	type Book = {
-		bookId?: number;
-		bookTitle?: string;
-		bookAuthorFirstName?: string;
-		bookAuthorLastName?: string;
-		genre?: string;
-		bookImg?: string;
-		publishedYear?: string;
-	};
